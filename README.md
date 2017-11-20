bb6d96e8cfb1fb973af38a5e86b6e8cc43c1fce1
php-cuong <bestearnmoney87@gmail.com>
2017-11-20 16:58:58 +0700

    init first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # magento2-express-shipping
 How to create a new shipping method in Magento 2
+
+# See the video How to create a new shipping method in Magento 2 here
+https://www.youtube.com/watch?v=0CzMIMyxaZQ
---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @Author: Ngo Quang Cuong
+ * @Date:   2017-11-20 16:16:48
+ * @Last Modified by:   https://www.facebook.com/giaphugroupcom
+ * @Last Modified time: 2017-11-20 16:16:58
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'PHPCuong_ExpressShipping',
+    __DIR__
+);
